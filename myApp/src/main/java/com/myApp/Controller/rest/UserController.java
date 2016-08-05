b180a569d6053470381189ccf387d4eea0eb9203
sahul chowdary <sahulkodali@gmail.com>
2016-08-05 16:02:10 -0500

    comitting controller

---
+++ myApp/src/main/java/com/myApp/Controller/rest/UserController.java
@@ -0,0 +1,28 @@
+package com.myApp.Controller.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myApp.Repositories.UserRepo;
+import com.myApp.model.Userdetails;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+	@Autowired
+	private UserRepo userRepo;
+	
+	public ResponseEntity<Userdetails>createCourse(@RequestBody Userdetails user){
+		Userdetails savedUser=userRepo.save(user);
+		return new ResponseEntity<Userdetails>(savedUser,HttpStatus.CREATED);
+		
+	}
+	
+	
+
+}
